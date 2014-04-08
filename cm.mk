683dc611146476de60b848f26dd6a229cfa1624e
John Kirk <jkirk777@gmail.com>
2014-04-08 01:47:29 -0600

    update build fingerprint bootloader name

--- cm.mk
+++ cm.mk
@@ -1,12 +1,9 @@
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 $(call inherit-product, device/samsung/lt02ltespr/full_lt02ltespr.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lt02ltespr TARGET_DEVICE=lt02ltespr BUILD_FINGERPRINT="samsung/lt02ltespr/lt02ltespr:4.2.2/JDQ39/T217SVPUAMH9:user/release-keys" PRIVATE_BUILD_DESC="lt02ltespr-user 4.2.2 JDQ39 T217SVPUAMH9 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lt02ltespr TARGET_DEVICE=lt02ltespr BUILD_FINGERPRINT="samsung/lt02ltespr/lt02ltespr:4.2.2/JDQ39/T217SVPUANB:user/release-keys" PRIVATE_BUILD_DESC="lt02ltespr-user 4.2.2 JDQ39 T217SVPUANB release-keys"
 
 PRODUCT_DEVICE := lt02ltespr
 PRODUCT_NAME := cm_lt02ltespr

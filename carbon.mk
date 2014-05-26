f1f0c6c80a14b0302f60a4eccb7639e4145595b7
John Kirk <jkirk777@gmail.com>
2014-05-25 19:19:02 -0600

    We build CARBON snitches!!!

--- carbon.mk
+++ carbon.mk
@@ -1,9 +1,8 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/carbon/config/common_full_tablet_wifionly.mk)
 
 $(call inherit-product, device/samsung/lt02ltespr/full_lt02ltespr.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lt02ltespr TARGET_DEVICE=lt02ltespr BUILD_FINGERPRINT="samsung/lt02ltespr/lt02ltespr:4.2.2/JDQ39/T217SVPUANB:user/release-keys" PRIVATE_BUILD_DESC="lt02ltespr-user 4.2.2 JDQ39 T217SVPUANB release-keys"
 
 PRODUCT_DEVICE := lt02ltespr
-PRODUCT_NAME := cm_lt02ltespr
+PRODUCT_NAME := carbon_lt02ltespr
